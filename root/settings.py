5d3491d5ddcd70ff38e80bb455a9a405a225fdde
jacurlive <jasurkkirchh@gmail.com>
2025-08-04 02:25:27 +0500

    update dependencies and docker compose

--- root/settings.py
+++ root/settings.py
@@ -10,7 +10,11 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
+import os
 from pathlib import Path
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -78,10 +82,15 @@ WSGI_APPLICATION = 'root.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('POSTGRES_DB', 'yt_db'),
+        'USER': os.environ.get('POSTGRES_USER', 'yt_user'),
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD', 'yt_pass'),
+        'HOST': os.environ.get('POSTGRES_HOST', 'localhost'),
+        'PORT': os.environ.get('POSTGRES_PORT', '5432'),
     }
 }
+
 
 
 # Password validation
